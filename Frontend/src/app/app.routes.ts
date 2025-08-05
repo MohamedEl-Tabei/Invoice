7ecc58ccb25fcc1e93b09856425462f5f509fc04
MohamedEl-Tabei <mohamedeltab33y@gmail.com>
2025-08-05 22:30:51 +0300

    router

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './Pages/home/home';
+import { Login } from './Pages/login/login';
+import { Register } from './Pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home ,title: 'Home' },
+    { path: 'login', component: Login ,title: 'Login' },
+    { path: 'signUp', component: Register, title: 'Sign Up' },
+];
